40da67e3d22b39d13a3d9a13afc73b87a9f13f2e
Danila <danila.ban@mail.ru>
2023-02-27 12:11:32 +0300

    feat: solve task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.midValue = Math.round(this.minValue + (this.maxValue - this.minValue) / 2);
+        return this.midValue;
     }
 
     lower() {
-
+        this.maxValue = this.midValue;
     }
 
     greater() {
-
+        this.minValue = this.midValue;
     }
 }
 
